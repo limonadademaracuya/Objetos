57d27b214f795dbdb9130316f17b0bfb4388d094
limonadademaracuya <126106463+limonadademaracuya@users.noreply.github.com>
2024-04-04 13:10:32 -0300

    Add files via upload

---
+++ Clase 9/src/activiadesPracticasNro1/punto10/Ejercicio10.java
@@ -0,0 +1,12 @@
+
+package activiadesPracticasNro1.punto10;
+
+
+public class Ejercicio10 {
+
+    public void (String[] args) {
+        
+    }
+    
+    
+}
---
+++ Clase 9/src/activiadesPracticasNro1/punto6/Gato.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package activiadesPracticasNro1.punto6;
+
+/**
+ *
+ * @author Alumno 4
+ */
+public class Gato {
+        
+    //atributos
+    private String nombre;
+    private String raza;
+    private String color;
+
+    //construtores
+    public Gato() {}
+
+    public Gato(String nombre, String raza, String color) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.color = color;
+    }
+
+
+    //getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+
+    //métodos
+    public void maullar () {
+
+    }
+    public void caminar () {
+
+    }
+    public void saltar () {
+
+    }
+    public void jugar () {
+
+    }
+
+
+
+    
+}
